389e17d02cc2abd5d729c3565ca688675d7f84c9
youngjuseo <julieseo2014@gmail.com>
2026-02-09 06:24:37 +0900

    SW: 항상 최신 버전 우선 로드 + 캐시 전략 변경

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-const CACHE_NAME = 'study-timer-v3';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE_NAME = 'study-timer-v4';
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
